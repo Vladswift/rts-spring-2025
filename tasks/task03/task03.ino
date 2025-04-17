8868fb6f76a1efd5cd28e43cf911690900a97529
Vladswift <borisovvladislav341@yandex.ru>
2025-04-17 00:36:40 -0700

    Update task03.ino

--- tasks/task03/task03.ino
+++ tasks/task03/task03.ino
@@ -1,7 +1,47 @@
-void setup() {
+volatile unsigned long long previousMicros = 0;
+volatile unsigned long long totalInterval = 0;
+volatile unsigned long long totalSquaredInterval = 0;
+volatile unsigned long long sampleCount = 0;
+const unsigned long long maxSampleSize = 1000;
 
-}
+void setup() {
+  Serial.begin(9600);
+  pinMode(3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(3), recordInterval, HIGH);
+ }
 
 void loop() {
+  if (sampleCount >= maxSampleSize) {
+    noInterrupts();
+    unsigned long long sumIntervals = totalInterval;
+    unsigned long long sumSquaredIntervals = totalSquaredInterval;
+    unsigned long long totalSamples = sampleCount;
+    totalInterval = 0;
+    totalSquaredInterval = 0;
+    sampleCount = 0;   
+    interrupts();
+   
 
+    float average = sumIntervals / totalSamples;
+    float variance = (sumSquaredIntervals / totalSamples) - (average * average);
+    unsigned long long standardDeviation = sqrt(variance);
+
+    Serial.print("Average: ");
+    Serial.print((float)average);
+    Serial.print(" us, StdDev: ");
+    Serial.print((float)standardDeviation);
+    Serial.println(" us");
+
+  }
+}
+
+void recordInterval() {
+  unsigned long long currentMicros = micros();
+  unsigned long long interval = currentMicros - previousMicros;
+
+  totalInterval += interval;
+  totalSquaredInterval += interval * interval;
+  sampleCount++;
+
+  previousMicros = currentMicros;
 }
